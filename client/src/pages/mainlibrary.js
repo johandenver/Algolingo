1d01498234a010389f9657e529994426dbbace9c
JohanDenver <48187072+johandenver@users.noreply.github.com>
2019-08-12 20:14:16 -0700

    Updated styling, updated sideNav, updates to algorithmCard

--- client/src/pages/mainlibrary.js
+++ client/src/pages/mainlibrary.js
@@ -1,32 +1,48 @@
 import React from "react";
 import Sidenav from "../components/sidenav";
 // import Container from "../components/container";
+// import { getFromStorage } from "../utils/storage";
 import AlgorithmCard from "../components/algorithmCard";
 
-function Library() {
-  return (
-    <div class="row">
-      <div class="col s12 m4 l3">
-        <Sidenav />
-      </div>
+class Library extends React.Component {
+  state = {
+    algorithms: []
+  };
 
-      <div class="col s12 m8 l9">
-        <AlgorithmCard />
-        <AlgorithmCard />
-        <AlgorithmCard />
-        <AlgorithmCard />
-        <AlgorithmCard />
-        <AlgorithmCard />
-        <AlgorithmCard />
-        <AlgorithmCard />
-        {/* <Container>
-          <h3>Algorithm Library</h3>
-          <div class="divider" />
-          <p>--Apend drop down Cards Here-- (with Algorithms from Database)</p>
-        </Container> */}
+  // componentDidMount(){
+  //   this.loadAlgorithms();
+  // }
+
+  // loadAlgorithms = () => {
+  //   getFromStorage.getFromStorage()
+  //   .then(res => this.setState({ algorithms: res.data }))
+  //   .catch(err => console.log(err));
+  // };
+
+  render() {
+    return (
+      <div class="row">
+        <div class="col s12 m4 l3">
+          <Sidenav />
+        </div>
+
+        <div class="col s12 m8 l9">
+          <h3>Algorithms</h3>
+          <br />
+          {this.state.algorithms.length ? (
+            <list>
+              {this.state.books.map(algorithm => (
+                <AlgorithmCard key={algorithm.id} />
+              ))}
+            </list>
+          ) : (
+            <h4>No Results to Display</h4>
+          )}
+          <AlgorithmCard />
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Library;
