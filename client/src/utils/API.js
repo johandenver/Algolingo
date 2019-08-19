c611d91a095de42311d441274237f19a1675a6eb
lynne.kwik@gmail.com <lynne.kwik@gmail.com>
2019-08-18 17:20:33 -0700

    updated the API.js for axios endpoints

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -5,6 +5,12 @@ export default {
 	// Gets all algorithms from Algorithms collection
 	getAlgorithms: function() {
 		return axios.get("/api/library");
+	},
+	getUserId: function(token) {
+		return axios.get("/api/user/token/" + token);
+	},
+	getUser: function(id) {
+		return axios.get("/api/user/" + id);
 	}
 };
 
