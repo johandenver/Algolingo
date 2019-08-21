42343b5329bc1db856aa31bdb7b77a7fb34dc0ae
lynne.kwik@gmail.com <lynne.kwik@gmail.com>
2019-08-20 21:21:21 -0700

    fixed findUserId method

--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const userController = require("../../../controllers/userController");
 
 // matches with /api/user/:id
-router.route("token/:token").get(userController.findUserId);
+router.route("/token/:token").get(userController.findUserId);
 router
 	.route("/:id")
 	.get(userController.findUserInfo)
