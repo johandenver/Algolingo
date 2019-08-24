7a4bd3670af7cae5c232de9648c54c2588b5dabe
lynne.kwik@gmail.com <lynne.kwik@gmail.com>
2019-08-24 08:21:20 -0700

    configs

--- server/server.js
+++ server/server.js
@@ -31,10 +31,15 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  app.get("*", function(req, res) {
+    const index = path.join(__dirname, "build", "index.html");
+    res.sendFile(index);
   });
 }
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+//   });
+// }
 
 // API routes
 app.use(routes);
