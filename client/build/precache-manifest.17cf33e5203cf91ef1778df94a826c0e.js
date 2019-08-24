0a798017db2b73cb2b819d37157eb146b6adeec2
lynne.kwik@gmail.com <lynne.kwik@gmail.com>
2019-08-24 08:26:00 -0700

    fixes

---
+++ client/build/precache-manifest.17cf33e5203cf91ef1778df94a826c0e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47e6c73d625206104153df7c66f08d33",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ad05b28b4f420e657ffa",
+    "url": "/static/css/2.58af2553.chunk.css"
+  },
+  {
+    "revision": "81247a7b897cdd7c8a8d",
+    "url": "/static/css/main.2fa02f2a.chunk.css"
+  },
+  {
+    "revision": "ad05b28b4f420e657ffa",
+    "url": "/static/js/2.71b1c159.chunk.js"
+  },
+  {
+    "revision": "81247a7b897cdd7c8a8d",
+    "url": "/static/js/main.122811a3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ef8aada412d18041cb5222136bddbc6f",
+    "url": "/static/media/AlgoLingo1.ef8aada4.png"
+  },
+  {
+    "revision": "9c34dd1136ec0f92b8cf774cec7380f6",
+    "url": "/static/media/AlgoLingo2.9c34dd11.png"
+  }
+]);
